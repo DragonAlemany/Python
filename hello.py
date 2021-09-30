635af8f2bbe144a8680aa520622e2d79813ca7eb
Jojo <phantasmasage@outlook.com>
2021-09-30 20:37:50 +0000

    Linuxiscooltoo

--- hello.py
+++ hello.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
 for line in range(5):
-    print("hello")    
+    print("hello world")    
     
 for line in range(3):
     print("Python is fun")
+
+for line in range(2):
+    print("Linux is cool too")
